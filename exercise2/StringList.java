db171275941c751a2a09d0317f0b7c45fc38cca9
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-09-06 22:03:33 +0200

    fairly complete list example

--- exercise2/StringList.java
+++ exercise2/StringList.java
@@ -1,3 +1,10 @@
+/**
+   A list of strings. At the beginning of exercise 2, StringList is
+   singly linked and does no error checking.
+   
+   <b>Update this documentation</b> when you are done with the
+   exercise.
+*/
 public class StringList
 {
     /**
@@ -6,61 +13,122 @@ public class StringList
     */
     private StringListNode head;
     
+    // Add this field when changing StringList into a doubly-linked list
+    //
+    // private StringListNode tail;
     
+    
+    /**
+       Insert a given value at the front of the list.
+    */
     public void pushFront(String value)
     {
-	// Write a method which adds the given value to the front of
-	// the list.
+	StringListNode node = new StringListNode(value);
+	node.next = head;
+	head = node;
     }
     
     
+    /**
+       Delete the first list item. This will result in the second item
+       becoming the new head, unless of course there are no more
+       elements. <b>No error checking</b> is performed, calling
+       popFront on an empty list will result in a null pointer
+       exception. Use the empty() to check for emptyness.
+    */
     public void popFront()
     {
-	// Write a method which removes the first element from the
-	// list.
+	head = head.next;
     }
     
     
+    /**
+       Retrieve the value which is stored in the first element of the
+       list. <b>No error checking</b> is performed: use the empty()
+       method to find out whether there is anything to retrieve.
+    */
     public String front()
     {
-	// Write a method which returns the string stored at the
-	// beginning of the list.
-	return "IMPLEMENT ME!";
+	return head.value;
     }
+
+    
+    // Add this method when changing StringList into a doubly-linked list
+    //
+    // public void pushBack(String value)
+    // {
+    // }
     
     
-    public void insertAfter(String value, StringListIterator position)
+    // Add this method when changing StringList into a doubly-linked list
+    //
+    // public void popBack()
+    // {
+    // }
+    
+    
+    // Add this method when changing StringList into a doubly-linked list
+    //
+    // public String back()
+    // {
+    // }
+    
+    
+    public void insert(String value, StringListIterator position)
     {
 	// Write a method which inserts the given value such that it
 	// ends up as the next element after the given position.
+	System.out.println("IMPLEMENT the insert method.");
+	System.exit(42);
     }
     
     
+    public void remove(StringListIterator position)
+    {
+	// Write a method which removes the node which comes
+	// immediately after the given position.
+	System.out.println("IMPLEMENT the remove method.");
+	System.exit(42);
+    }
+    
+    
+    /**
+       Determines whether the list is empty.
+    */
     public boolean empty()
     {
-	// Write a method which determines whether the StringList is
-	// currently empty.
-	return true;	      // this is just a placeholder for now...
+	return null == head;
     }
     
     
+    /**
+       Removes all elements from the list.
+    */
     public void clear()
     {
-	// Write a method which removes all elements from the list.
+	head = null;
     }
     
     
+    /**
+       Creates an iterator that starts at the beginning of the list.
+    */
     public StringListIterator begin()
     {
 	return new StringListIterator(head);
     }
     
     
+    /**
+       Prints all elements of the list on a separate line of
+       System.out. Each line is prefixed with the given string to
+       allow a basic form of custom formatting.
+    */
     public void print(String prefix)
     {
-       // Write a method which prints all elements of the list, each
-       // on a separate line. Each line should begin with the prefix
-       // given as argument.
+	for (StringListNode current = head; null != current; current = current.next) {
+	    System.out.println(prefix + current.value);
+	}
     }
     
     
@@ -72,12 +140,16 @@ public class StringList
 	
 	StringList sl = new StringList();
 	
+	System.out.println("pushing some things onto the list...");
+	
 	sl.pushFront("hello");
 	for (int ii = 0; ii < 3; ++ii) {
 	    sl.pushFront(" " + ii);
 	}
 	sl.pushFront("byebye!");
-	sl.print("* ");
+	
+	System.out.println("result:");
+	sl.print("  * ");
 	
 	if (sl.empty()) {
 	    System.out.println("the StringList is empty");
@@ -86,8 +158,12 @@ public class StringList
 	    System.out.println("the StringList is not empty");
 	}
 	
+	System.out.println("clearing the list...");
+
 	sl.clear();
-	sl.print("* ");
+
+	System.out.println("result:");
+	sl.print("  * ");
 	
 	if (sl.empty()) {
 	    System.out.println("the StringList is empty");
@@ -95,20 +171,24 @@ public class StringList
 	else {
 	    System.out.println("the StringList is not empty");
 	}
+	
+	System.out.println("pushing some things onto the list...");
 	
 	sl.pushFront("one");
 	sl.pushFront("two");
 	sl.pushFront("three");
+
+	System.out.print("using the iterator:");
 	for (StringListIterator ii = sl.begin(); ii.valid(); ii.next()) {
 	    System.out.print(" " + ii.get());
 	}
 	System.out.println();
 	
-	sl.print("* ");
 	while ( ! sl.empty()) {
-	    System.out.println("  pop " + sl.front());
+	    System.out.println("popping " + sl.front());
 	    sl.popFront();
-	    sl.print("  - ");
+	    System.out.println("result:");
+	    sl.print("  * ");
 	}
     }
 }
