f2f637789db07420cf7175a396c519d2fc641d62
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-10-13 09:53:25 +0200

    example with functor... much nicer actually than the comparable sublcass

--- project2/PriorityQueueExample.java
+++ project2/PriorityQueueExample.java
@@ -73,6 +73,9 @@ public class PriorityQueueExample
     
     public static void main(String[] args)
     {
+	//////////////////////////////////////////////////
+	// initialize some data
+	
 	SomeData[] data = new SomeData[20];
 	for (int ii = 0; ii < data.length; ++ii) {
 	    StringBuilder sb = new StringBuilder();
@@ -82,10 +85,21 @@ public class PriorityQueueExample
 	    data[ii] = new SomeData(rnd.nextDouble() * 200.0 - 100.0, sb.toString());
 	}
 	
+	//////////////////////////////////////////////////
+	// demo of priority queue based on subclassing the java.lang.Comparable interface
+	
 	System.out.println("Example 1: using a Comparable with an extra 'value' field");
+	
+	// NOTE: the queue stores ExampleOne objects, which contain
+	//       the SomeData object inside of them
 	PriorityQueue<ExampleOne> pqOne = new PriorityQueue<ExampleOne>();
 	for (int ii = 0; ii < data.length; ++ii) {
 	    int value = rnd.nextInt(5);
+	    // NOTE: each queue entry gets its own new wrapper object;
+	    //       we could also make SomeData a subclass of
+	    //       Comparable of course, but then we would not be
+	    //       able to use different comparison approaches
+	    //       depending on the needs of the application
 	    if (pqOne.add(new ExampleOne(value, data[ii]))) {
 		System.out.println("  inserted " + data[ii] + " (value " + value
 				   + ")\ttop: " + pqOne.peek());
@@ -102,5 +116,36 @@ public class PriorityQueueExample
 	    }
 	    System.out.println("  " + ee);
 	}
+	System.out.println("NOTE: the above list goes by decreasing (value X)");
+	
+	//////////////////////////////////////////////////
+	// demo of priority queue based on a separate java.util.Comparator functor
+	
+	System.out.println("\nExample 2: using a Comparator");
+	
+	// NOTE: the queue stores SomeData objects directly, the
+	//       comparator is a separate functor object; the
+	//       PriorityQueue constructor which takes the extra
+	//       comparator argument also needs to receive an initial
+	//       capacity
+	PriorityQueue<SomeData> pqTwo = new PriorityQueue<SomeData>(11, new ExampleTwo());
+	for (int ii = 0; ii < data.length; ++ii) {
+	    // NOTE: we add data object directly, no need for wrapper objects
+	    if (pqTwo.add(data[ii])) {
+		System.out.println("  inserted " + data[ii] + "\ttop: " + pqTwo.peek());
+	    }
+	    else {
+		System.out.println("  FAILED to insert " + data[ii]);
+	    }
+	}
+	System.out.println("Emptying the queue:");
+	while (true) {
+	    SomeData sd = pqTwo.poll();
+	    if (null == sd) {
+		break;
+	    }
+	    System.out.println("  " + sd);
+	}
+	System.out.println("NOTE: the above list goes by decreasing number after the /");
     }
 }
