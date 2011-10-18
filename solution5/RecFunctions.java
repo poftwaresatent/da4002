174b33414528e177c7ea4282c3c180a0da19c9fa
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-10-18 15:49:11 +0200

    implemented the recursive function examples (with a modest amount of sanity checking)

--- solution5/RecFunctions.java
+++ solution5/RecFunctions.java
@@ -1,76 +1,111 @@
 class RecFunctions{
-
-    /*
-      This class includes a number of functions that should be
-      implemented recursively. It has the purpose of providing you
-      with material for training your understanding of recursion. In
-      many cases the best solution might not be recursive, but here
-      you are asked to give a recursive method!
-
-      Think of the rules from the book:
-      
-           Always at least one base case!
-           The recursive calls should make progress towards the base case!
-           Trust that the recursive call will do what is expected!
-     */
-
-    
-
-    public static long sum(int n){
-        // sum all integer numbers from 0 to n.
-        // sum(0) = 0
-        // sum(n) = n + sum(n-1) for n > 0.
-        // What happens when you call your function with a negative argument?
-        // What do you think should happen?
-	return 0;
-    }
-
-
-    public static long sum(int a, int b){
-        // sum all integer numbers from a to b inclusive. b
-        // sum(a,a) = a
-        // sum(a,b) = b + sum(a, b-1) for b > a.
-        // What happens when you call your function with b < a?
-        // What do you think should happen?
-	return 0;
-    }
-
-    
-    public static long sumOdds(int n){
-        // sum the first n positive odd numbers 1, 3, 5, 7.  Try this
-        // function for some inputs. What can you say about the
-        // results? Could you compute them in a faster way?
-        // Do you think this is possible for other sums?
-	return 0;
-    }
-
-    public static double power(double base, int exponent){
-        // raise base to the exponent.
-        // power(base,0) = 1
-        // power(base,n) = base * power(base, n-1) for n >0.
-            // What happens when you call your function with a negative argument?
-        // What do you think should happen?
-	return 0;
-    }
-
-    public static boolean palindrome(String str){
-        // str is a palindrome if it can be read alike forwards and
-        // backwards.  Examples of palindromes are ANA, ANANA Can you
-        // modify your program so that it ignores white spaces, so
-        // that even sentences can be considered. Here comes a longer
-        // spanish palindrome: DABALE ARROZ A LA ZORRA EL ABAD. And
-        // one in english: A SANTA LIVED AS A DEVIL AT NASA.  Does
-        // your function consider it a palindrome?
-	return false;
+    // The palindrome detector needs to be able to quickly look up
+    // whitespace characters. So, we store them in a hash table.
+    private static java.util.HashSet<Character> whitespace = new java.util.HashSet<Character>();
+    static {
+	String ws = " ,.;:-_'\"\t\n?!";
+	for (int ii = 0; ii < ws.length(); ++ii) {
+	    whitespace.add(ws.charAt(ii));
+	}
     }
     
-    public static void main(String[] cmdLn){
+    
+    public static long sum(int n)
+    {
+	if (n <= 0) {
+	    return 0;
+	}
+	return n + sum(n-1);
+    }
+    
+    
+    public static long sum(int a, int b)
+    {
+	if (b < a) {
+	    return sum(b, a);
+	}
+	if (a == b) {
+	    return a;
+	}
+	return b + sum(a, b-1);
+    }
+    
+    
+    public static long sumOdds(int n)
+    {
+	if (n <= 1) {
+	    return 1;
+	}
+	return 2*n-1 + sumOdds(n-1);
+    }
+    
+    
+    public static double power(double base, int exponent)
+    {
+	if (exponent <= 0) {
+	    return 1;
+	}
+	return base * power(base, exponent-1);
+    }
+    
+    
+    private static boolean palindromeRec(char[] str, int first, int last)
+    {
+	if (first >= last) {
+	    return true;
+	}
+	if (str[first] != str[last]) {
+	    return false;
+	}
+	for (int ii = first + 1; first < str.length; ++ii) {
+	    if ( ! whitespace.contains(str[ii])) {
+		first = ii;
+		break;
+	    }
+	    if (first >= last) {
+		return true;
+	    }
+	}
+	for (int ii = last - 1; last >= 0; --ii) {
+	    if ( ! whitespace.contains(str[ii])) {
+		last = ii;
+		break;
+	    }
+	    if (first >= last) {
+		return true;
+	    }
+	}
+	return palindromeRec(str, first, last);
+    }
+    
+    
+    public static boolean palindrome(String str)
+    {
+	return palindromeRec(str.toCharArray(), 0, str.length()-1);
+    }
+    
+    
+    public static void main(String[] cmdLn)
+    {
         System.out.println(sum(10));
-        System.out.println(sum(100,110));
+        System.out.println(sum(100, 110));
         System.out.println(sumOdds(4));
-        System.out.println(power(2,10));
-        System.out.println(palindrome("anana"));
-    
+        System.out.println(power(2, 10));
+	
+	String[] examplePalindromes = {
+	    "ANA",
+	    "ANANA",
+	    "DABALE ARROZ A LA ZORRA EL ABAD",
+	    "I don't think this is a palidrome, do you?",
+	    "A SANTA LIVED AS A DEVIL AT NASA"
+	};
+	for (int ii = 0; ii < examplePalindromes.length; ++ii) {
+	    if (palindrome(examplePalindromes[ii])) {
+		System.out.println("Yes, this is a palindrome: " + examplePalindromes[ii]);
+	    }
+	    else {
+		System.out.println("No, this is not a palindrome: " + examplePalindromes[ii]);
+	    }
+	}
     }
-
 }
