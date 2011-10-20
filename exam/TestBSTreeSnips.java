c70cb0d6f0259a89ba66a5c7ff870d44b1469ae7
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-10-20 15:51:48 +0200

    first round of feedback from slawomir. now on to the last two questions...

--- exam/TestBSTreeSnips.java
+++ exam/TestBSTreeSnips.java
@@ -9,8 +9,35 @@ public class TestBSTreeSnips {
 	}
     }
     
+    static void printDot(Node root) {
+	System.out.println("digraph bstree {\n  graph [overlap=scale];");
+	java.util.LinkedList<Node> queue = new java.util.LinkedList<Node>();
+	queue.offer(root);
+	while (0 < queue.size()) {
+	    Node node = queue.pop();
+	    if (null != node.foo) {
+		System.out.println("  " + node.value + " -> " + node.foo.value + " [len=2];");
+		queue.offer(node.foo);
+	    }
+	    if (null != node.bar) {
+		System.out.println("  " + node.value + " -> " + node.bar.value + " [len=2];");
+		queue.offer(node.bar);
+	    }
+	}
+	System.out.println("}");
+    }
+    
     public static void main(String[] args) {
-	long[] values = { 5, 3, 13, -7, 3, 7, 42, 3, 5 };
+	long[] values = { 5, 3, 13, -7, 3, 7, 42, 4, 3 };
+	boolean verbose = true;
+	
+	if (0 < args.length) {
+	    verbose = false;
+	    values = new long[args.length];
+	    for (int ii = 0; ii < args.length; ++ii) {
+		values[ii] = Integer.parseInt(args[ii]);
+	    }
+	}
 	
 	ContainerOne bstree = new ContainerOne();
 	ContainerTwo dblist = new ContainerTwo();
@@ -19,17 +46,21 @@ public class TestBSTreeSnips {
 	    dblist.insert(values[ii]);
 	}
 	
-	System.out.println("binary search tree (in order):");
-	printInOrder(bstree.handle);
-	
-	System.out.println("\ndoubly linked list (forward):");
-	for (Node ii = dblist.alpha; ii != null; ii = ii.foo) {
-	    System.out.println(ii.value);
+	if (verbose) {
+	    System.out.println("binary search tree (in order):");
+	    printInOrder(bstree.handle);
 	}
+	printDot(bstree.handle);
 	
-	System.out.println("\ndoubly linked list (reverse):");
-	for (Node ii = dblist.beta; ii != null; ii = ii.bar) {
-	    System.out.println(ii.value);
+	if (verbose) {
+	    System.out.println("\ndoubly linked list (forward):");
+	    for (Node ii = dblist.alpha; ii != null; ii = ii.foo) {
+		System.out.println(ii.value);
+	    }
+	    System.out.println("\ndoubly linked list (reverse):");
+	    for (Node ii = dblist.beta; ii != null; ii = ii.bar) {
+		System.out.println(ii.value);
+	    }
 	}
     }
 }
