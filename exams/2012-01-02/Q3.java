137089639e60007d589237a549663f7f07f83b7f
Roland Philippsen <roland dot philippsen at gmx dot net>
2011-12-19 18:56:30 +0100

    done with graph stuff (draft anyway), will need to add something about dynamic programming next

--- exams/2012-01-02/Q3.java
+++ exams/2012-01-02/Q3.java
@@ -1,33 +1,18 @@
-import java.util.*;
+import java.util.LinkedList;
 
 class Vertex {
-    final String name;
-    LinkedList<Vertex> neighbors;
-    
-    Vertex(String _name) {
-	name = _name;
-	neighbors = new LinkedList<Vertex>();
-    }
+  LinkedList neighbors
+    = new LinkedList();
 }
 
 class Graph {
-    public HashMap<String, Vertex> vertices;
-    
-    public Graph()
-    {
-	vertices = new HashMap<String, Vertex>();
-	Vertex a = new Vertex("A");
-	Vertex b = new Vertex("B");
-	Vertex c = new Vertex("C");
-	Vertex d = new Vertex("D");
-	vertices.put("A", a);
-	vertices.put("B", b);
-	vertices.put("C", c);
-	vertices.put("D", d);
-	a.neighbors.add(b);
-	a.neighbors.add(c);
-	b.neighbors.add(c);
-	b.neighbors.add(d);
-	d.neighbors.add(a);
-    }
+  Vertex A, B, C, D;
+  
+  public Graph() {
+    A.neighbors.add(B);
+    A.neighbors.add(C);
+    B.neighbors.add(C);
+    B.neighbors.add(D);
+    D.neighbors.add(A);
+  }
 }
