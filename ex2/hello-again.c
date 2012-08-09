47018ce9d4a8ee37b8f221e6701b7fa3b68bf5d8
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-08-09 23:19:54 +0200

    started writing it down but got sidetracked again, too many other things (sigh)

---
+++ ex2/hello-again.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void say_hello()
+{
+  printf("hello, world\n");
+}
+
+double celsius_to_fahrenheit(double temperature)
+{
+  return BLAH;
+}
+
+
+int main(int argc, char **argv)
+{
+  say_hello();
+  return 0;
+}
