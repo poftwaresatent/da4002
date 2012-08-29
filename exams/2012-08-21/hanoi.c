a1d70ae689027ab9b2593583edcf42103ae177af
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-08-29 16:40:57 +0200

    some improvements for exam solution based on grading of Ye Tang (student in Shanghai)... thanks a lot!

--- exams/2012-08-21/hanoi.c
+++ exams/2012-08-21/hanoi.c
@@ -15,17 +15,40 @@ char other (char src, char dst)
 }
 
 
-int move (int size, char src, char dst)
+int move (int verbose, int size, char src, char dst)
 {
   if (1 >= size) {
-    printf ("disk 1 from %c to %c\n", src, dst);
+    if (verbose) {
+      printf ("disk 1 from %c to %c\n", src, dst);
+    }
     return 1;
   }
 
   int cnt = 1;
-  cnt += move(size-1, src, other(src, dst));
-  printf ("disk %d from %c to %c\n", size, src, dst);
-  cnt += move(size-1, other(src, dst), dst);
+  cnt += move(verbose, size-1, src, other(src, dst));
+  if (verbose) {
+    printf ("disk %d from %c to %c\n", size, src, dst);
+  }
+  cnt += move(verbose, size-1, other(src, dst), dst);
+  return cnt;
+}
+
+
+int move2 (int verbose, int size, char aa, char bb, char cc)
+{
+  if (1 >= size) {
+    if (verbose) {
+      printf ("disk 1 from %c to %c\n", aa, cc);
+    }
+    return 1;
+  }
+  
+  int cnt = 1;
+  cnt += move2(verbose, size-1, aa, cc, bb);
+  if (verbose) {
+    printf ("disk %d from %c to %c\n", size, aa, cc);
+  }
+  cnt += move2(verbose, size-1, bb, aa, cc);
   return cnt;
 }
 
@@ -35,7 +58,7 @@ int main (int argc, char ** argv)
   int size;
   for (size = 1; size < 6; ++size) {
     printf ("\nsize %d:\n", size);
-    printf ("hanoi size %d takes %d moves\n", size, move(size, 'A', 'B'));
+    printf ("hanoi size %d takes %d moves\n", size, move2(1, size, 'A', 'B', 'C'));
   }
   return 0;
 }
