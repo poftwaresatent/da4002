939ac072947abe9ab97c3537fd3ca87701a391d0
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-09-16 13:11:20 +0200

    forgot to add file

---
+++ ex4/pointer-to-pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int getptr (int guess, double ** data)
+{
+  static double value = 42.0;
+  if (42 != guess)
+    return -1;
+  *data = &value;
+  return 0;
+}
+
+int main (int argc, char ** argv)
+{
+  double * ptr;
+  int guess = 42;
+  if (0 == getptr (guess, &ptr))
+    printf ("guess = %d   pointer = %p   value = %f\n", guess, ptr, *ptr);
+  else
+    printf ("it's not at %d...\n", guess);
+}
