d00de196044638c8fce94105dfefcf65803ecf3e
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-09-17 10:29:54 +0200

    Very nice graphviz visualization of what's going on in a binary max heap as we insert and pop elements.

--- ex6/simple-int-heap.c
+++ ex6/simple-int-heap.c
@@ -7,7 +7,8 @@
 /*
   This simple implementation has a fixed capacity.
 */
-#define CAPACITY 1024
+#define CAPACITY  100
+#define BUFSIZE  1024
 
 
 typedef struct {
@@ -22,14 +23,58 @@ void init (Heap * heap)
 }
 
 
+void dump_dot (Heap * heap, FILE * of, char * label)
+{
+  int ii;
+  fprintf (of, "digraph \"Heap\" {\n  graph [label=\"%s\",overlap=scale];\n", label);
+  for (ii = 1; ii <= heap->len; ++ii) {
+    fprintf (of, "  %d [label=\"%d\"];\n", ii, heap->num[ii]);
+    if (ii * 2 > heap->len)
+      continue;
+    fprintf (of, "  %d -> %d;\n", ii, ii * 2);
+    if (ii * 2 >= heap->len)
+      continue;
+    fprintf (of, "  %d -> %d;\n", ii, ii * 2 + 1);
+  }
+  fprintf (of, "}\n");
+}
+
+
+void dump_dot_file (Heap * heap, char * post, char * label)
+{
+  static char const * pre = "dump";
+  static size_t count = 0;
+  FILE * of;
+  char buf[BUFSIZE];
+  
+  if (BUFSIZE <= snprintf (buf, BUFSIZE, "%s-%03lu-%s.dot", pre, count++, post))
+    errx (EXIT_FAILURE, "dump_dot_file: insufficient BUFSIZE");
+  if (NULL == (of = fopen (buf, "w")))
+    err (EXIT_FAILURE, "dump_dot_file: fopen %s", buf);
+  
+  dump_dot (heap, of, label);
+
+  if (EOF == fclose (of))
+    err (EXIT_FAILURE, "dump_dot_file: fclose %s", buf);
+}
+
+
 void bubble_up (Heap * heap, size_t index)
 {
-  size_t parent = index / 2;
+  size_t parent;
+  char buf[BUFSIZE];
+  
+  parent = index / 2;
   while ((parent > 0) && (heap->num[index] > heap->num[parent])) {
     int tmp;
     tmp = heap->num[index];
     heap->num[index] = heap->num[parent];
     heap->num[parent] = tmp;
+    
+    if (BUFSIZE <= snprintf (buf, BUFSIZE, "up %d (index %lu <-> %lu)", tmp, index, parent))
+      errx (EXIT_FAILURE, "bubble_up: insufficient BUFSIZE");
+    dump_dot_file (heap, "up", buf);
+    
     index = parent;
     parent = index / 2;
   }
@@ -39,6 +84,8 @@ void bubble_up (Heap * heap, size_t index)
 void bubble_down (Heap * heap, size_t index)
 {
   size_t left, right, target;
+  char buf[BUFSIZE];
+  
   target = index;
   for (;;) {
     int tmp;
@@ -53,6 +100,11 @@ void bubble_down (Heap * heap, size_t index)
     tmp = heap->num[index];
     heap->num[index] = heap->num[target];
     heap->num[target] = tmp;
+    
+    if (BUFSIZE <= snprintf (buf, BUFSIZE, "down %d (index %lu <-> %lu)", tmp, index, target))
+      errx (EXIT_FAILURE, "bubble_up: insufficient BUFSIZE");
+    dump_dot_file (heap, "up", buf);
+    
     index = target;
   }
 }
@@ -60,9 +112,16 @@ void bubble_down (Heap * heap, size_t index)
 
 void insert (Heap * heap, int num)
 {
+  char buf[BUFSIZE];
+  
   if (CAPACITY <= heap->len)
     errx (EXIT_FAILURE, "ERROR: heap is full");
   heap->num[++heap->len] = num; /* pre-increment because num starts at [1] */
+  
+  if (BUFSIZE <= snprintf (buf, BUFSIZE, "inserted %d (at %lu)", num, heap->len))
+    errx (EXIT_FAILURE, "insert: insufficient BUFSIZE");
+  dump_dot_file (heap, "ins", buf);
+  
   bubble_up (heap, heap->len);
 }
 
@@ -70,16 +129,31 @@ void insert (Heap * heap, int num)
 int pop (Heap * heap)
 {
   int num;
+  char buf[BUFSIZE];
+  
   if (0 == heap->len)
     errx (EXIT_FAILURE, "ERROR: pop called on empty heap");
   num = heap->num[1];		/* remember, we start at [1] instead of [0] */
   heap->num[1] = heap->num[heap->len--];
-  bubble_down (heap, 1);
+  
+  if (0 < heap->len) {
+    if (BUFSIZE <= snprintf (buf, BUFSIZE, "popped %d (replaced by %d)", num, heap->num[1]))
+      errx (EXIT_FAILURE, "pop: insufficient BUFSIZE");
+    dump_dot_file (heap, "pop", buf);
+    
+    bubble_down (heap, 1);
+  }
+  else {
+    if (BUFSIZE <= snprintf (buf, BUFSIZE, "popped %d (last item)", num))
+      errx (EXIT_FAILURE, "pop: insufficient BUFSIZE");
+    dump_dot_file (heap, "pop", buf);
+  }
+  
   return num;
 }
 
 
-void dump (Heap * heap)
+void dump (Heap * heap, FILE * of)
 {
   size_t ii;
   /*
@@ -87,8 +161,8 @@ void dump (Heap * heap)
     (one), instead of the usual 0 (zero).
   */
   for (ii = 1; ii <= heap->len; ++ii)
-    fprintf (stderr, " %3d", heap->num[ii]);
-  fprintf (stderr, "\n");
+    fprintf (of, " %3d", heap->num[ii]);
+  fprintf (of, "\n");
 }
 
 
@@ -104,27 +178,16 @@ int main (int argc, char ** argv)
       errx (EXIT_FAILURE, "failed to parse integer from argument %d `%s'", ii, argv[ii]);
     insert (&heap, num);
     fprintf (stderr, "inserted %d\n  ", num);
-    dump (&heap);
+    dump (&heap, stderr);
   }
   
-  printf ("digraph \"Heap\" {\n  graph [label=\"%s", argv[1]);
-  for (ii = 2; ii < argc; ++ii)
-    printf (" %s", argv[ii]);
-  printf ("\",overlap=scale];\n");
-  for (ii = 1; ii <= heap.len; ++ii) {
-    printf ("  %d [label=\"%d\"];\n", ii, heap.num[ii]);
-    if (ii * 2 > heap.len)
-      continue;
-    printf ("  %d -> %d;\n", ii, ii * 2);
-    printf ("  %d -> %d;\n", ii, ii * 2 + 1);
-  }
-  printf ("}\n");
+  dump_dot (&heap, stdout, "FOO");
   
   fprintf (stderr,"--------------------------------------------------\n");
   while (0 < heap.len) {
     int num = pop (&heap);
     fprintf (stderr, "popped %d\n  ", num);
-    dump (&heap);
+    dump (&heap, stderr);
   }
 
   return 0;
