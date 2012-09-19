a469572a4e397939861226a627d98bc5e42c21cb
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-09-19 22:37:14 +0200

    okay done with strheap

--- ex6/test-strheap.c
+++ ex6/test-strheap.c
@@ -10,22 +10,24 @@ int main (int argc, char ** argv)
   int ii;
   StrHeap *heap;
   
+  strheap_debug_on ();
+  
   heap = strheap_new ();
   
   for (ii = 1; ii < argc; ++ii) {
     char * str;
     if (NULL == (str = strdup (argv[ii])))
-      err (EXIT_FAILURE, "strdup failed on argument %d `%s'", ii, argv[ii]);
+      err (EXIT_FAILURE, __FILE__": strdup failed on argument %d `%s'", ii, argv[ii]);
     strheap_insert (heap, str);
-    fprintf (stderr, "inserted %s\n  ", str);
-    strheap_dump (heap, stderr);
+    fprintf (stdout, "inserted %s\n  ", str);
+    strheap_dump (heap, stdout);
   }
   
-  fprintf (stderr,"--------------------------------------------------\n");
+  fprintf (stdout,"--------------------------------------------------\n");
   while (0 < heap->len) {
     char * str = strheap_extract (heap);
-    fprintf (stderr, "extracted %s\n  ", str);
-    strheap_dump (heap, stderr);
+    fprintf (stdout, "extracted %s\n  ", str);
+    strheap_dump (heap, stdout);
     free (str);
   }
   
