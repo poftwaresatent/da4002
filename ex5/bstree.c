33dbd37d57a757b7e77e60b275687312b61c4b03
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-09-19 13:40:05 +0200

    keep things simple here for now

--- ex5/bstree.c
+++ ex5/bstree.c
@@ -10,13 +10,19 @@
 
 
 typedef struct bsitem_s {
-  void * data;
+  int data;
   struct bsitem_s * left;
   struct bsitem_s * right;
 } BSItem;
 
 
-BSItem * bsitem_new (void * data)
+/*
+ * Helper function to create a new item for a binary search tree. Note
+ * that in this simple example, we will never give back the memory
+ * allocated here. But any real implementation would need to properly
+ * manage its memory, of course.
+ */
+BSItem * bsitem_new (int data)
 {
   BSItem * item;
   if (NULL == (item = calloc (sizeof(*item), 1)))
@@ -26,141 +32,167 @@ BSItem * bsitem_new (void * data)
 }
 
 
-void bsitem_delete_rec (BSItem * item, void (*data_delete)(void*))
-{
-  if (NULL == item)
-    return;
-  bsitem_delete_rec (item->left, data_delete);
-  bsitem_delete_rec (item->right, data_delete);
-  if (NULL != data_delete)
-    data_delete (item->data);
-  free (item);
-}
-
-
-void bsitem_app_io_rec (BSItem * item, void (*fct)(BSItem*))
-{
-  if (NULL == item)
-    return;
-  bsitem_app_io_rec (item->left, fct);
-  fct (item);
-  bsitem_app_io_rec (item->right, fct);
-}
-
-
 /* ==================================================
    Binary Search Tree
    ================================================== */
 
 
 typedef struct bstree_s {
+  /*
+    It's a bit silly to have a struct which contains just one pointer,
+    but a more general implementation of binary search trees will have
+    more structure members.
+  */
   BSItem * root;
-  int (*data_cmp)(void*, void*);
-  void (*data_delete)(void*);
 } BSTree;
 
 
-BSTree * bstree_new (int (*data_cmp)(void*, void*), void (*data_delete)(void*))
+/*
+ * User function to create a new tree. Usually you would have to pass
+ * custom functions for deleting data and for comparing data, and
+ * there would also be a bstree_delete function to clean up after you
+ * are done with the tree. But in this simple example here we skip all
+ * of that.
+ */
+BSTree * bstree_new ()
 {
   BSTree * tree;
   if (NULL == (tree = calloc (sizeof(*tree), 1)))
-    err (EXIT_FAILURE, "bstree_new: calloc");
-  tree->data_cmp = data_cmp;
-  tree->data_delete = data_delete;
+    err (EXIT_FAILURE, "bstree_new: malloc");
   return tree;
 }
 
 
-void bstree_delete (BSTree * tree)
+/*
+ * Helper function for inserting an item into a tree. Users will
+ * simply call the bstree_ins function instead (without the "_rec"
+ * suffix). This function does one of four things:
+ *
+ *  1. If an empty slot has been found, create a new item for the
+ *     data, to attach it as a new leaf to the tree.
+ *
+ *  2. If the given data is smaller than the one already stored in the
+ *     root, then recurse into the left subtree for insertion there.
+ *
+ *  3. If the data is bigger than what we already have, recurse into
+ *     the right subtree instead.
+ *
+ *  4. Otherwise, the data is already stored in the tree, so we do
+ *     nothing.
+ *     NOTE that, in a more general setting, if the data compares as
+ *     equal to what is stored, it could still be different. In that
+ *     case the course of action depends on what is desired of the
+ *     tree implementation, but four basic options exist:
+ *      a. Ignore the new data and keep the old.
+ *      b. Delete the old data, replace it with the new.
+ *      c. Treat this as an error.
+ *      d. Append the new data to a list stored in the item.
+ */
+BSItem * bstree_ins_rec (BSTree * tree, BSItem * root, int data)
 {
-  if (NULL != tree->root) {
-    bsitem_delete_rec (tree->root, tree->data_delete);
-    tree->root = NULL;
-  }
-  free (tree);
-}
-
-
-BSItem * bstree_ins_rec (BSTree * tree, BSItem * root, void * data)
-{
-  int cmp;
-  
   if (NULL == root)
     return bsitem_new (data);
   
-  cmp = tree->data_cmp(data, root->data);
-  if (0 > cmp)			/* it's smaller than our data */
+  if (data < root->data)
     root->left = bstree_ins_rec (tree, root->left, data);
-  else if (0 < cmp)		/* it's bigger than our data */
+  else if (data > root->data)
     root->right = bstree_ins_rec (tree, root->right, data);
-  else {			/* it's equal to our data */
-    if (NULL != tree->data_delete)
-      tree->data_delete (root->data);
-    root->data = data;
-  }
+  /*
+    else ignore: we already have this data
+  */
   
   return root;
 }
 
 
-void bstree_ins (BSTree * tree, void * data)
+/*
+ * User function to insert data (in this simple example that's just a
+ * number) into the binary search tree.
+ */
+void bstree_ins (BSTree * tree, int data)
 {
   tree->root = bstree_ins_rec (tree, tree->root, data);
 }
 
 
-BSItem * bstree_rem_rec (BSTree * tree, BSItem * root, void * data)
+/*
+ * Helper function for removing an item from the tree. Users will
+ * simply call the bstree_rem function instead (without the "_rec"
+ * suffix).
+ *
+ * This function takes the root of a subtree as argument, and returns
+ * the item which should replace that subtree. It is intended to be
+ * called as follows:
+ * \code
+ *   tree->root = bstree_rem_rec (tree, tree->root, data);
+ * \endcode
+ *
+ * In most cases, the returned item will be just the root passed in as
+ * argument. But if the given root matches the data and one or both of
+ * its subtrees are NULL, then something else is returned.  This
+ * function does one of the following:
+ *
+ *  1. If the data is smaller than what is stored in this root,
+ *     recurse into the left subtree.
+ *
+ *  2. If the data is bigger, then recurse to the right.
+ *
+ *  3. If the data matches and this root is a leaf (has no children),
+ *     then simply remove it. NOTE that this is handled transparently
+ *     by the code which handles possibility #4.
+ *
+ *  4. If the data matches and this root has only one subtree (either
+ *     left or right), then use the "parent re-attachment trick" to
+ *     skip over us.
+ *
+ *  5. If none of the above can be used, then we find the smallest
+ *     child of the right subtree, and replace this root with that
+ *     smallest child. It would work just as well to use the biggest
+ *     child of the left subtree.
+ *
+ * NOTE again that we do not properly manage memory in this simple
+ * implementation. This function should actually call a user-supplied
+ * function to give back the memory used by the, and free the item
+ * which gets removed from the tree.
+ */
+BSItem * bstree_rem_rec (BSTree * tree, BSItem * root, int data)
 {
-  int cmp;
+  BSItem *child, *parent;
   
   if (NULL == root)
     return NULL;
   
-  cmp = tree->data_cmp(data, root->data);
-  if (0 > cmp) {
-    /* remove it from the smaller subtree */
+  if (data < root->data) {
     root->left = bstree_rem_rec (tree, root->left, data);
     return root;
   }
-  if (0 < cmp) {
-    /* remove it from the bigger subtree */
+  if (data > root->data) {
     root->right = bstree_rem_rec (tree, root->right, data);
     return root;
   }
   
   /*
-    root matches the data, thus remove "this" item (we may end up
-    replacing our data and removing another one, depending on context)
-  */
-  
-  if (NULL != tree->data_delete) {
-    tree->data_delete (root->data);
-  }
+   * We have to remove this root. Depending on context, we'll solve
+   * this by removing another item after saving its data here.
+   */
   
   if (NULL == root->left) {
-    /* easy: there is no smaller subtree, use the "parent
-       re-attachment trick" (replace this root with its bigger
-       subtree, which may be NULL here by the way) */
-    BSItem *tmp;
-    tmp = root->right;
-    free (root);
-    return tmp;
+    /* there is no left subtree: use the "parent re-attachment trick"
+       to replace this root with its right subtree (which may be NULL,
+       in which case this root was a leaf node) */
+    return root->right;
   }
   if (NULL == root->right) {
     /* the other way around is just as easy... */
-    BSItem *tmp;
-    tmp = root->left;
-    free (root);
-    return tmp;
+    return root->left;
   }
   
   /*
-    this root has two subtrees, which is trickier... we replace its
-    data with the smallest child of its bigger subtree, and remove
-    that smallest child instead (the other way around would work as
-    well, using the biggest child of the smaller subtree).
-  */
-  BSItem *child, *parent;
+   * We have two subtrees, that's slightly trickier: replace this root
+   * with the smallest child of its bigger subtree, and remove that
+   * smallest child instead.
+   */
+  
   parent = root;
   child = root->right;
   while (NULL != child->left) {
@@ -169,29 +201,28 @@ BSItem * bstree_rem_rec (BSTree * tree, BSItem * root, void * data)
   }
   root->data = child->data;
   
-  /* one final potential issue: if root->right has no smaller subtree,
-     we have to reattach it's bigger subtree as root->right (same as
-     parent->right in this case) */
+  /*
+    One final potential issue: if root->right has no smaller subtree,
+    we have to reattach it's bigger subtree as root->right (same as
+    parent->right in this case).
+  */
+  
   if (parent == root)
     parent->right = child->right;
   else
     parent->left = child->right;
   
-  free (child);
-  
   return root;
 }
 
 
-void bstree_rem (BSTree * tree, void * data)
+/*
+ * User function to actually remove some data. This silently ignore
+ * data that is not actually stored in the tree.
+ */
+void bstree_rem (BSTree * tree, int data)
 {
   tree->root = bstree_rem_rec (tree, tree->root, data);
-}
-
-
-void bstree_app_io (BSTree * tree, void (*fct)(BSItem*))
-{
-  bsitem_app_io_rec (tree->root, fct);
 }
 
 
@@ -200,25 +231,23 @@ void bstree_app_io (BSTree * tree, void (*fct)(BSItem*))
    ================================================== */
 
 
-int int_cmp (void * lhs, void * rhs)
-{
-  return *(int*)lhs - *(int*)rhs;
-}
-
-
-void int_item_print_dot (BSItem * item)
-{
-  if (NULL != item->left)
-    printf ("  \"%d\" -> \"%d\" [label=\"l\"];\n", *(int*)item->data, *(int*)item->left->data);
-  if (NULL != item->right)
-    printf ("  \"%d\" -> \"%d\" [label=\"r\"];\n", *(int*)item->data, *(int*)item->right->data);
-}
-
-
 typedef enum {
   INSERT,
   REMOVE
 } pstate_t;
+
+
+void print_dot (BSItem * item)
+{
+  if (NULL != item->left) {
+    print_dot (item->left);
+    printf ("  \"%d\" -> \"%d\" [label=\"l\"];\n", item->data, item->left->data);
+  }
+  if (NULL != item->right) {
+    printf ("  \"%d\" -> \"%d\" [label=\"r\"];\n", item->data, item->right->data);
+    print_dot (item->right);
+  }
+}
 
 
 int main (int argc, char ** argv)
@@ -230,7 +259,7 @@ int main (int argc, char ** argv)
   if (2 > argc)
     errx (EXIT_FAILURE, "Please provide a sequence of insertions and removals.");
   
-  tree = bstree_new (int_cmp, free);
+  tree = bstree_new ();
   
   ps = INSERT;
   for (ii = 1; ii < argc; ++ii) {
@@ -239,17 +268,13 @@ int main (int argc, char ** argv)
     else if ('R' == toupper(argv[ii][0]))
       ps = REMOVE;
     else {
-      int *num;
-      if (NULL == (num = malloc (sizeof(*num))))
-	err (EXIT_FAILURE, "failed to allocate integer");
-      if (1 != sscanf (argv[ii], "%d", num))
+      int num;
+      if (1 != sscanf (argv[ii], "%d", &num))
 	errx (EXIT_FAILURE, "failed to parse integer from argument %d `%s'", ii, argv[ii]);
       if (INSERT == ps)
 	bstree_ins (tree, num);
-      else {
+      else
 	bstree_rem (tree, num);
-	free (num);
-      }
     }
   }
   
@@ -257,9 +282,8 @@ int main (int argc, char ** argv)
   for (ii = 2; ii < argc; ++ii)
     printf (" %s", argv[ii]);
   printf ("\",overlap=scale];\n");
-  bstree_app_io (tree, int_item_print_dot);
+  print_dot (tree->root);
   printf ("}\n");
   
-  bstree_delete (tree);
   return 0;
 }
---
+++ scratch/generic-bstree.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <err.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/* ==================================================
+   Binary Search Tree Item
+   ================================================== */
+
+
+typedef struct bsitem_s {
+  void * data;
+  struct bsitem_s * left;
+  struct bsitem_s * right;
+} BSItem;
+
+
+BSItem * bsitem_new (void * data)
+{
+  BSItem * item;
+  if (NULL == (item = calloc (sizeof(*item), 1)))
+    err (EXIT_FAILURE, "bsitem_new: malloc");
+  item->data = data;
+  return item;
+}
+
+
+void bsitem_delete_rec (BSItem * item, void (*data_delete)(void*))
+{
+  if (NULL == item)
+    return;
+  bsitem_delete_rec (item->left, data_delete);
+  bsitem_delete_rec (item->right, data_delete);
+  if (NULL != data_delete)
+    data_delete (item->data);
+  free (item);
+}
+
+
+void bsitem_app_io_rec (BSItem * item, void (*fct)(BSItem*))
+{
+  if (NULL == item)
+    return;
+  bsitem_app_io_rec (item->left, fct);
+  fct (item);
+  bsitem_app_io_rec (item->right, fct);
+}
+
+
+/* ==================================================
+   Binary Search Tree
+   ================================================== */
+
+
+typedef struct bstree_s {
+  BSItem * root;
+  int (*data_cmp)(void*, void*);
+  void (*data_delete)(void*);
+} BSTree;
+
+
+BSTree * bstree_new (int (*data_cmp)(void*, void*), void (*data_delete)(void*))
+{
+  BSTree * tree;
+  if (NULL == (tree = calloc (sizeof(*tree), 1)))
+    err (EXIT_FAILURE, "bstree_new: calloc");
+  tree->data_cmp = data_cmp;
+  tree->data_delete = data_delete;
+  return tree;
+}
+
+
+void bstree_delete (BSTree * tree)
+{
+  if (NULL != tree->root) {
+    bsitem_delete_rec (tree->root, tree->data_delete);
+    tree->root = NULL;
+  }
+  free (tree);
+}
+
+
+BSItem * bstree_ins_rec (BSTree * tree, BSItem * root, void * data)
+{
+  int cmp;
+  
+  if (NULL == root)
+    return bsitem_new (data);
+  
+  cmp = tree->data_cmp(data, root->data);
+  if (0 > cmp)			/* it's smaller than our data */
+    root->left = bstree_ins_rec (tree, root->left, data);
+  else if (0 < cmp)		/* it's bigger than our data */
+    root->right = bstree_ins_rec (tree, root->right, data);
+  else {			/* it's equal to our data */
+    if (NULL != tree->data_delete)
+      tree->data_delete (root->data);
+    root->data = data;
+  }
+  
+  return root;
+}
+
+
+void bstree_ins (BSTree * tree, void * data)
+{
+  tree->root = bstree_ins_rec (tree, tree->root, data);
+}
+
+
+BSItem * bstree_rem_rec (BSTree * tree, BSItem * root, void * data)
+{
+  int cmp;
+  
+  if (NULL == root)
+    return NULL;
+  
+  cmp = tree->data_cmp(data, root->data);
+  if (0 > cmp) {
+    /* remove it from the smaller subtree */
+    root->left = bstree_rem_rec (tree, root->left, data);
+    return root;
+  }
+  if (0 < cmp) {
+    /* remove it from the bigger subtree */
+    root->right = bstree_rem_rec (tree, root->right, data);
+    return root;
+  }
+  
+  /*
+    root matches the data, thus remove "this" item (we may end up
+    replacing our data and removing another one, depending on context)
+  */
+  
+  if (NULL != tree->data_delete) {
+    tree->data_delete (root->data);
+  }
+  
+  if (NULL == root->left) {
+    /* easy: there is no smaller subtree, use the "parent
+       re-attachment trick" (replace this root with its bigger
+       subtree, which may be NULL here by the way) */
+    BSItem *tmp;
+    tmp = root->right;
+    free (root);
+    return tmp;
+  }
+  if (NULL == root->right) {
+    /* the other way around is just as easy... */
+    BSItem *tmp;
+    tmp = root->left;
+    free (root);
+    return tmp;
+  }
+  
+  /*
+    this root has two subtrees, which is trickier... we replace its
+    data with the smallest child of its bigger subtree, and remove
+    that smallest child instead (the other way around would work as
+    well, using the biggest child of the smaller subtree).
+  */
+  BSItem *child, *parent;
+  parent = root;
+  child = root->right;
+  while (NULL != child->left) {
+    parent = child;
+    child = child->left;
+  }
+  root->data = child->data;
+  
+  /* one final potential issue: if root->right has no smaller subtree,
+     we have to reattach it's bigger subtree as root->right (same as
+     parent->right in this case) */
+  if (parent == root)
+    parent->right = child->right;
+  else
+    parent->left = child->right;
+  
+  free (child);
+  
+  return root;
+}
+
+
+void bstree_rem (BSTree * tree, void * data)
+{
+  tree->root = bstree_rem_rec (tree, tree->root, data);
+}
+
+
+void bstree_app_io (BSTree * tree, void (*fct)(BSItem*))
+{
+  bsitem_app_io_rec (tree->root, fct);
+}
+
+
+/* ==================================================
+   Test and Debug
+   ================================================== */
+
+
+int int_cmp (void * lhs, void * rhs)
+{
+  return *(int*)lhs - *(int*)rhs;
+}
+
+
+void int_item_print_dot (BSItem * item)
+{
+  if (NULL != item->left)
+    printf ("  \"%d\" -> \"%d\" [label=\"l\"];\n", *(int*)item->data, *(int*)item->left->data);
+  if (NULL != item->right)
+    printf ("  \"%d\" -> \"%d\" [label=\"r\"];\n", *(int*)item->data, *(int*)item->right->data);
+}
+
+
+typedef enum {
+  INSERT,
+  REMOVE
+} pstate_t;
+
+
+int main (int argc, char ** argv)
+{
+  int ii;
+  pstate_t ps;
+  BSTree * tree;
+  
+  if (2 > argc)
+    errx (EXIT_FAILURE, "Please provide a sequence of insertions and removals.");
+  
+  tree = bstree_new (int_cmp, free);
+  
+  ps = INSERT;
+  for (ii = 1; ii < argc; ++ii) {
+    if ('I' == toupper(argv[ii][0]))
+      ps = INSERT;
+    else if ('R' == toupper(argv[ii][0]))
+      ps = REMOVE;
+    else {
+      int *num;
+      if (NULL == (num = malloc (sizeof(*num))))
+	err (EXIT_FAILURE, "failed to allocate integer");
+      if (1 != sscanf (argv[ii], "%d", num))
+	errx (EXIT_FAILURE, "failed to parse integer from argument %d `%s'", ii, argv[ii]);
+      if (INSERT == ps)
+	bstree_ins (tree, num);
+      else {
+	bstree_rem (tree, num);
+	free (num);
+      }
+    }
+  }
+  
+  printf ("digraph \"BSTree\" {\n  graph [label=\"%s", argv[1]);
+  for (ii = 2; ii < argc; ++ii)
+    printf (" %s", argv[ii]);
+  printf ("\",overlap=scale];\n");
+  bstree_app_io (tree, int_item_print_dot);
+  printf ("}\n");
+  
+  bstree_delete (tree);
+  return 0;
+}
