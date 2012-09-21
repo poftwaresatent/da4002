8464ce7356478fb7dd84e28d529d122709bf1d76
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-09-22 00:13:30 +0200

    added argument parsing and a one-liner to summarize the data parameters

--- proj1-sol/main-rnd.c
+++ proj1-sol/main-rnd.c
@@ -40,15 +40,37 @@ int main (int argc, char ** argv)
 {
   int urnd;
   uint len = 1000;		/* total data length */
-  uint chmin = 10;		/* minimum sorted chunk size */
-  uint chmax = 200;		/* maximum sorted chunk size */
+  uint chmin = 1;		/* minimum sorted chunk size */
+  uint chmax = 1;		/* maximum sorted chunk size */
   char chdir = 'a';		/* ascending or descending chunks */
   /* uint swmin = 2;		/\* minimum number of swapped pairs *\/ */
   /* uint swmax = 5;		/\* maximum number of swapped pairs *\/ */
   uint nrem;
   
+  if (1 < argc && 1 != sscanf (argv[1], "%u", &len))
+    errx (EXIT_FAILURE, "invalid length `%s'", argv[1]);
+  if (2 < argc && 1 != sscanf (argv[2], "%u", &chmin))
+    errx (EXIT_FAILURE, "invalid minimum chunk size `%s'", argv[2]);
+  if (3 < argc && 1 != sscanf (argv[3], "%u", &chmax))
+    errx (EXIT_FAILURE, "invalid maximum chunk size `%s'", argv[3]);
+  if (4 < argc) {
+    switch (argv[4][0]) {
+    case 'a': case 'A': chdir = 'a'; break;
+    case 'd': case 'D': chdir = 'd'; break;
+    default:
+      errx (EXIT_FAILURE, "invalid sort direction `%s'", argv[4]);
+    }
+  }
+  
+  if (0 == chmin)
+    chmin = 1;
+  if (chmax < chmin)
+    chmax = chmin;
+  
   if (0 > (urnd = open ("/dev/urandom", O_RDONLY)))
     err (EXIT_FAILURE, "open /dev/urandom");
+  
+  printf ("# %u %u %u %c\n", len, chmin, chmax, chdir);
   
   for (nrem = len; 0 < nrem; /**/) {
     uint ch, jj;
