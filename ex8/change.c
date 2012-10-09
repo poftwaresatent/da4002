45025e41d784c4d5700fafdb046424c81ec5d25b
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-10-09 23:44:43 +0200

    big changes for ex 8.2.2 and 8.2.3, now it is generic and reads command line arguments

--- ex8/change.c
+++ ex8/change.c
@@ -1,63 +1,120 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
 
-#define KMAX 20
 
 int main (int argc, char ** argv)
 {
+  int ndenom;
+  int kmax;
+  int *denom;
   struct {
-    int v1, v3, v7, best;
-  } tab[KMAX+1];
-  int ii;
+    int *value;
+    int *count;
+    int best;
+  } *tab;
+  int ii, jj;
   
-  tab[0].v1 = -1;;
-  tab[0].v3 = -1;;
-  tab[0].v7 = -1;;
+  ndenom = 0;
+  kmax = 0;
+  denom = NULL;
+  if (argc > 1) {
+    if (1 != sscanf (argv[1], "%d", &kmax))
+      errx (EXIT_FAILURE, "arg 1 `%s': positive int expected", argv[1]);
+    if (kmax <= 0)
+      errx (EXIT_FAILURE, "arg 1 (%d): positive int expected", kmax);
+  }
+  for (ii = 2; ii < argc; ++ii) {
+    if (1 != sscanf (argv[ii], "%d", &jj))
+      errx (EXIT_FAILURE, "arg %d `%s': positive int expected", ii, argv[ii]);
+    if (jj <= 0)
+      errx (EXIT_FAILURE, "arg %d (%d): positive int expected", ii, jj);
+    ++ndenom;
+    if (NULL == (denom = realloc (denom, ndenom * sizeof *denom)))
+      err (EXIT_FAILURE, "realloc");
+    denom[ndenom-1] = jj;
+  }
+  
+  if (kmax == 0)
+    kmax = 20;
+  if (ndenom == 0) {
+    ndenom = 3;
+    denom = malloc (ndenom * sizeof *denom);
+    denom[0] = 1;
+    denom[1] = 3;
+    denom[2] = 7;
+  }
+  
+  tab = malloc ((kmax+1) * sizeof *tab);
+  for (ii = 0; ii <= kmax; ++ii) {
+    tab[ii].value = calloc (ndenom, sizeof(int));
+    tab[ii].count = calloc (ndenom, sizeof(int));
+  }
+  
   tab[0].best = 0;
   
-  /*       12 | 12 12 12 | 12 | 1 2 3 */
-  printf (" K | C1 C2 C3 |  V | best coin(s)\n"
-	  "---+----------+----+-------------\n");
+  for (ii = 1; ii <= kmax; ++ii) {
+    
+    for (jj = 0; jj < ndenom; ++jj) {
+      tab[ii].count[jj] = 0;
+      if (denom[jj] > ii)
+	tab[ii].value[jj] = -1;
+      else {
+	tab[ii].value[jj] = 1 + tab[ii - denom[jj]].best;
+	if (0 == jj)
+	  tab[ii].best = tab[ii].value[jj];
+	else if (tab[ii].best > tab[ii].value[jj])
+	  tab[ii].best = tab[ii].value[jj];
+      }
+    }
+    
+    for (jj = ndenom - 1; jj >= 0; --jj)
+      if (tab[ii].best == tab[ii].value[jj]) {
+	int kprev = ii - denom[jj];
+	tab[ii].count[jj] = 1;
+	for (jj = 0; jj < ndenom; ++jj)
+	  tab[ii].count[jj] += tab[kprev].count[jj];
+	break;
+      }
+  }
   
-  for (ii = 1; ii <= KMAX; ++ii) {
-    printf ("%2d | ", ii);
-    
-    tab[ii].v1 = 1 + tab[ii-1].best;
-    tab[ii].best = tab[ii].v1;
-    printf ("%2d ", tab[ii].v1);
-    
-    if (ii < 3) {
-      tab[ii].v3 = -1;
-      printf ("   ");
+  printf ("  K |");
+  for (ii = 0; ii < ndenom; ++ii)
+    printf (" C%-2d", denom[ii]);
+  printf (" |  V |  choices and optimal sequence\n"
+	  "----------------------------------------------------------------------\n");
+  
+  for (ii = 1; ii <= kmax; ++ii) {
+    char sep;
+    printf ("%3d |", ii);
+    for (jj = 0; jj < ndenom; ++jj)
+      if (0 < tab[ii].value[jj])
+	printf (" %3d", tab[ii].value[jj]);
+      else
+	printf ("    ");
+    printf (" | %2d |", tab[ii].best);
+    for (jj = 0; jj < ndenom; ++jj)
+      if (tab[ii].best == tab[ii].value[jj])
+	printf (" %2d", denom[jj]);
+      else
+	printf ("   ");
+    printf (" |");
+    for (jj = 0, sep = ' '; jj < ndenom; ++jj) {
+      int kk;
+      for (kk = 0; kk < tab[ii].count[jj]; ++kk) {
+	printf ("%c%d", sep, denom[jj]);
+	sep = '+';
+      }
     }
-    else {
-      tab[ii].v3 = 1 + tab[ii-3].best;
-      if (tab[ii].v3 < tab[ii].best)
-	tab[ii].best = tab[ii].v3;
-      printf ("%2d ", tab[ii].v3);
-    }
-    
-    if (ii < 7) {
-      tab[ii].v7 = -1;
-      printf ("   | ");
-    }
-    else {
-      tab[ii].v7 = 1 + tab[ii-7].best;
-      if (tab[ii].v7 < tab[ii].best)
-	tab[ii].best = tab[ii].v7;
-      printf ("%2d | ", tab[ii].v7);
-    }
-    
-    printf ("%2d | ", tab[ii].best);
-    
-    if (tab[ii].best == tab[ii].v1)
-      printf ("1 ");
-    if (tab[ii].best == tab[ii].v3)
-      printf ("3 ");
-    if (tab[ii].best == tab[ii].v7)
-      printf ("7 ");
-    
     printf ("\n");
   }
+  
+  for (ii = 0; ii <= kmax; ++ii) {
+    free (tab[ii].value);
+    free (tab[ii].count);
+  }
+  free (tab);
+  free (denom);
   
   return 0;
 }
