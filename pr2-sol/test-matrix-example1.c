b00984fe7fcc1a0a01c1e8e84483c4ebede8264e
Roland Philippsen <roland dot philippsen at gmx dot net>
2012-10-11 11:18:22 +0200

    Self-explaining use of a simple array to store a matrix. Interestingly, this code makes Valgrind crash. I should submit a bug report ;-)

---
+++ pr2-sol/test-matrix-example1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+
+int main (int argc, char ** argv)
+{
+  int nrows, ncols, *matrix;
+  int ir, ic;
+  
+  /*
+    Initialize and allocate a 5x7 matrix.
+  */
+  nrows = 5;
+  ncols = 7;
+  matrix = malloc (nrows * ncols * sizeof(int));
+  if (NULL == matrix)
+    err (EXIT_FAILURE, "malloc");
+  
+  /*
+    Fill the matrix with some example values. Notice that the array
+    index is one-dimensional: in order to access the element at
+    (ir,ic) you have to translate it into the index ir+ic*ncols.
+  */
+  for (ir = 0; ir < nrows; ++ir)
+    for (ic = 0; ic < ncols; ++ic)
+      if ((ir + ic) % 2)
+	matrix[ir + ic*ncols] = -10*ic - ir;
+      else
+	matrix[ir + ic*ncols] = 10*ir + ic;
+  
+  /*
+    Print the matrix as a nice table with a header row that lists the
+    column indices, a header column which shows the row indices, and
+    the values right-aligned into a field of width 3.
+  */
+  printf ("  |");
+  for (ic = 0; ic < ncols; ++ic)
+    printf (" %3d", ic);
+  printf ("\n--+");
+  for (ic = 0; ic < ncols; ++ic)
+    printf ("----");
+  printf ("\n");
+  for (ir = 0; ir < nrows; ++ir) {
+    printf ("%d |", ir);
+    for (ic = 0; ic < ncols; ++ic)
+      printf (" %3d", matrix[ir + ic*ncols]);
+    printf ("\n");
+  }
+  
+  /*
+    Free the memory used by the matrix.
+  */
+  free (matrix);
+  
+  return 0;
+}
