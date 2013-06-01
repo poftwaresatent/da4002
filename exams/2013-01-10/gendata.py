aa876262a61609690f8b71dd9be4aa8304c90391
Roland Philippsen <poftwaresatent@gmail.com>
2013-06-01 14:33:47 +0200

    I guess these were final changes to the exam, but that lies a while back so I cannot remember... anyway.

--- exams/2013-01-10/gendata.py
+++ exams/2013-01-10/gendata.py
@@ -9,30 +9,28 @@ def lb(x):
     return log(x) / LN2
 
 def fquad(n):
-    return 3e6 + 2e4*n + 3*n*n    + 3e3*n*lb(n) + 5e9*lb(n)
+    return 200 + 30*n + 0.3*n*n  + 5*n*lb(n)   + 500*lb(n)
 
 def fnln(n):
-    return 1e9 + 0.3*n            + 5*n*lb(n)   + 1e10*lb(n)
+    return 5000 + n            + 50*n*lb(n) + 10*lb(n)
 
-NSTART=1e2
-NMAX=1e7
-PFAC=1e-4
+NSTART=10
 
+NMAX=1e3*NSTART
 n = NSTART
 while n < NMAX:
     t = fquad(n)
-    print PFAC*n, t, t/pow(n,2), t/n/lb(n), t/n, t/lb(n)
+    print n, t, t/pow(n,2), t/n/lb(n), t/n, t/lb(n)
     n *= 1.1
 
 print
 print
 
-NSTART=1e7
-NMAX=1e12
-PFAC=1e-9
+#NSTART=90
 
+NMAX=1e3*NSTART
 n = NSTART
 while n < NMAX:
     t = fnln(n)
-    print PFAC*n, t, t/pow(n,2), t/n/lb(n), t/n, t/lb(n)
+    print n, t, t/pow(n,2), t/n/lb(n), t/n, t/lb(n)
     n *= 1.1
