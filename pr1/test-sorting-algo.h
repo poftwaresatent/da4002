92810ece378e38056910214fd5c6db26a5ac2bff
Roland Philippsen <poftwaresatent@gmail.com>
2013-11-11 15:23:23 +0100

    better as a separate {h,c} module

---
+++ pr1/test-sorting-algo.h
@@ -0,0 +1,8 @@
+#ifndef TEST_SORTING_ALGO_H
+#define TEST_SORTING_ALGO_H
+
+void test_create (int index, int ** input, int ** output);
+int test_check (int const * input, int * output);
+void test_destroy (int * input, int * output);
+
+#endif
